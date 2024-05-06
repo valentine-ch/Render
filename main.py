1405dc260fde87d661a043e218f707ca0d8dcb76
valentine-ch <valentinvchicherin@gmail.com>
2024-05-07 01:52:35 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import aiohttp
+import dotenv
+import os
+
+
+dotenv.load_dotenv()
+
+
+ADDRESS = os.getenv("ADDRESS")
+app = FastAPI()
+scheduler = AsyncIOScheduler()
+
+
+async def send_request():
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(url=ADDRESS)
+
+
+@app.on_event("startup")
+async def startup():
+    scheduler.add_job(send_request, "interval", minutes=2)
+    scheduler.start()
+
+
+@app.get("/ping")
+async def ping():
+    return "pong"
